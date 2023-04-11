8bed477ad7fdd73893a5f4da27a0ec9d3be7c545
wasif28 <wasif28@hotmail.com>
2023-04-11 12:53:12 +0500

    deployment scripts for parent chain

--- contracts/testing/ERC20Sample.sol
+++ contracts/testing/ERC20Sample.sol
@@ -3,10 +3,9 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TestToken is ERC20 { 
-
-    constructor () ERC20("testToken","testToken") {
-        _mint(msg.sender, 1000000 * (10**18));
+contract TestToken is ERC20 {
+    constructor() ERC20("testToken", "testToken") {
+        _mint(msg.sender, 1000000 * (10 ** 18));
     }
 
     function mint(uint256 amount) public {
--- contracts/testing/ERC721Sample.sol
+++ contracts/testing/ERC721Sample.sol
@@ -3,15 +3,14 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract TestNFT is ERC721 { 
-
-    uint256 public counter = 0; 
+contract TestNFT is ERC721 {
+    uint256 public counter = 0;
 
     function _baseURI() internal view virtual override returns (string memory) {
         return "LegacyBASEURI/";
     }
 
-    constructor () ERC721("testNFT","testNFT") {
+    constructor() ERC721("testNFT", "testNFT") {
         _mint(msg.sender, counter);
         counter++;
     }
